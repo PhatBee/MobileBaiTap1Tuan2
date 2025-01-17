925ba0aea11d2bc5b151084256d4227070ca337a
PhatBee <vinhphatst1235@gmail.com>
2025-01-17 08:38:31 +0700

    Nội dung 2 Thêm thư viện CircleImageView. Hình và thông tin của sinh viên, bo tròn hình

--- app/src/main/java/vn/phatbee/baitap01/MainActivity.java
+++ app/src/main/java/vn/phatbee/baitap01/MainActivity.java
@@ -1,6 +1,7 @@
 package vn.phatbee.baitap01;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +16,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        TextView textViewName = findViewById(R.id.textViewName);
+        textViewName.setText("Ong Vĩnh Phát \n" +
+                "Lớp: 221101ST1A \n" +
+                "MSSV: 22110394");
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
 }
